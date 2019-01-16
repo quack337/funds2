af8578e3eb1e7d6c377d23874cfc26228410fe98
Jiyunn <jygr0309@gmail.com>
2019-01-16 19:05:10 +0900

    직위 반환 메소드 추가. (#jiyun)

--- src/main/java/fund/dto/Certificate.java
+++ src/main/java/fund/dto/Certificate.java
@@ -6,32 +6,57 @@ import lombok.Data;
 @Data
 public class Certificate {
 
-    int id;
-    int type;
-    String certificateNo;
-    int userId;
-    String createDate;
-    String personNo;
-    String personName;
-    String department;
-    int amount;
-    String body;
-    String userName;
-    Integer corporateId;
-    String corporateName;
-    String representative;
-    
-    public String getAmountToKorean() {
-    	return NumberUtils.toKoreanNumber(amount, true, true);
-    }
-    
-    public String getRepresentativeWithBlank() {
-    	StringBuilder sb = new StringBuilder();
-    	
-    	for(int i=0; i<representative.length(); i++) {
-    		sb.append(representative.charAt(i));
-    		sb.append(" ");
-    	}
-    	return sb.toString();
-    }
+	int id;
+	int type;
+	String certificateNo;
+	int userId;
+	String createDate;
+	String personNo;
+	String personName;
+	String department;
+	int amount;
+	String body;
+	String userName;
+	Integer corporateId;
+	String corporateName;
+	String representative;
+
+	public String getAmountToKorean() {
+		return NumberUtils.toKoreanNumber(amount, true, true);
+	}
+
+	public String getCorporateWithBlank() {
+		StringBuilder sb = new StringBuilder();
+
+		for (int i = 0; i < corporateName.length(); i++) {
+			sb.append(corporateName.charAt(i));
+			sb.append("  ");
+		}
+		return sb.toString();
+	}
+
+	public String getRepresentativeWithBlank() {
+		StringBuilder sb = new StringBuilder();
+
+		for (int i = 0; i < representative.length(); i++) {
+			sb.append(representative.charAt(i));
+			sb.append("  ");
+		}
+		return sb.toString();
+	}
+
+	public String getStatus() {
+		switch (corporateId) {
+		case 1: {
+			return "총 장";
+		}
+		case 2: {
+			return "이 사 장";
+		}
+		case 3: {
+			return "단 장";
+		}
+		}
+		return "";
+	}
 }
